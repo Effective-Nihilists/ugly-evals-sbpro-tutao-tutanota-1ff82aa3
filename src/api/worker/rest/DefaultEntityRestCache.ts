608b1d127d068170b8db5e2594d666b2fb1f1a89
eval-harness <eval@ugly-studio.local>
2026-04-29 12:08:57 -0700

    eval: kimi-kimi-k2.6-1777487917

--- src/api/worker/rest/DefaultEntityRestCache.ts
+++ src/api/worker/rest/DefaultEntityRestCache.ts
@@ -162,6 +162,8 @@ export interface CacheStorage extends ExposedCacheStorage {
 	getUserId(): Id
 
 	deleteAllOwnedBy(owner: Id): Promise<void>
+
+	deleteLastBatchIdForGroup(groupId: Id): Promise<void>
 }
 
 /**
@@ -724,6 +726,7 @@ export class DefaultEntityRestCache implements EntityRestCache {
 		for (const ship of removedShips) {
 			console.log("Lost membership on ", ship._id, ship.groupType)
 			await this.storage.deleteAllOwnedBy(ship.group)
+			await this.storage.deleteLastBatchIdForGroup(ship.group)
 		}
 	}
 
